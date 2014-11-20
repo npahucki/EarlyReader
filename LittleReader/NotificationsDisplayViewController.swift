d55cc0a0f2a8f9663041dcb9f9a38946cbae3534
Nathan Pahucki <npahucki@gmail.com>
2014-11-20 19:26:20 -0300

    rename file

--- LittleReader/NotificationsDisplayViewController.swift
+++ LittleReader/NotificationsDisplayViewController.swift
@@ -25,7 +25,6 @@ class NotificationsDisplayViewController: UIViewController, ManagedObjectContext
     
     private var dirUp = true
     private var _controllers = [NotificationViewController]()
-    private var fetchedResultsController = NSFetchedResultsController()
     
     var managedContext : NSManagedObjectContext? = nil
     var delegate : NotificationsDisplayViewControllerDelegate?
