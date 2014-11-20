05680758e5c0359d8e7c180183242d20e3dab7b1
Nathan Pahucki <npahucki@gmail.com>
2014-11-20 18:08:33 -0300

    WIP

--- LittleReader/NotificationViewController.swift
+++ LittleReader/NotificationViewController.swift
@@ -10,7 +10,18 @@ import UIKit
 
 class NotificationViewController: UIViewController {
 
+    private var _notification : Notification?
     
+    var notification : Notification? {
+        get {
+            return _notification
+        }
+        set {
+            _notification = notification
+            updateStateForNotification()
+        }
+        
+    }
     
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var typeLabel: UILabel!
@@ -18,6 +29,18 @@ class NotificationViewController: UIViewController {
     
     
     @IBAction func didClickCloseNotification(sender: AnyObject) {
+        if let parentVc = parentViewController as? NotificationsDisplayViewController {
+            parentVc.removeLastNotificaitonViewController()
+        }
     }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    private func updateStateForNotification() {
+        // TODO: Update the UI!
+    }
+    
     
 }
