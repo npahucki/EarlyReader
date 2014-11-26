221e1c22d4ebdab682838e05472a0f0157e3ffb8
Nathan Pahucki <npahucki@gmail.com>
2014-11-26 19:21:16 -0300

    Next step in project rename.

--- LittleReader/ObjCBridging.h
+++ LittleReader/ObjCBridging.h
@@ -1,13 +1,13 @@
 //
 //  ObjCBridging.h
-//  LittleReader
+//  EarlyReader
 //
 //  Created by Nathan  Pahucki on 10/24/14.
 //  Copyright (c) 2014 Nathan Pahucki. All rights reserved.
 //
 
-#ifndef LittleReader_ObjCBridging_h
-#define LittleReader_ObjCBridging_h
+#ifndef EarlyReader_ObjCBridging_h
+#define EarlyReader_ObjCBridging_h
 
 #import "NSDate+HumanizedTime.h"
 #import "STCollapseTableView.h"
